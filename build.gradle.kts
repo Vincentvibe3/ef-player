cef7f14ad241ee4c3fb3af43d96a7b5339cbdd00
Vincent <vincentvibe3@gmail.com>
2022-01-01 19:44:16 -0500

    add publishing to build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.10"
-    application
+    `maven-publish`
 }
 
-group = "io.github.vincentvibe3.webm"
+group = "io.github.vincentvibe3.EfPlayer"
 version = "1.0"
 
 repositories {
@@ -24,6 +24,20 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
-application {
-    mainClass.set("MainKt")
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/Vincentvibe3/ef-player")
+            credentials {
+                username = System.getenv("USERNAME")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 }
