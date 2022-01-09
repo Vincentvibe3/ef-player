68a3450550e2dc3311ae4b369d257ca5078f9ba7
Vincent <vincentvibe3@gmail.com>
2022-01-08 20:13:40 -0500

    add docs build action

--- src/main/kotlin/com/github/Vincentvibe3/efplayer/core/EventListener.kt
+++ src/main/kotlin/com/github/Vincentvibe3/efplayer/core/EventListener.kt
@@ -2,20 +2,69 @@ package com.github.Vincentvibe3.efplayer.core
 
 abstract class EventListener() {
 
+    /**
+     * Called when a track gets loaded
+     *
+     * @param track The track that is loaded
+     * @param player The player that called the function
+     */
     open fun onTrackLoad(track: Track, player: Player){}
 
+    /**
+     * Called when a playlist is loaded
+     *
+     * @param tracks A list of <a href="#{@link}">{@link Track}</a> that were loaded
+     * @param player The player that called the function
+     */
     open fun onPlaylistLoaded(tracks: List<Track>, player: Player){}
 
+    /**
+     * Called when a track gets loaded
+     *
+     * @param track The track that is loaded
+     * @param player The player that called the function
+     */
     open fun onTrackStart(track: Track, player: Player){}
 
+    /**
+     * Called when a track finishes playing
+     *
+     * @param track The <a href="#{@link}">{@link Track}</a> that is done
+     * @param player The player that called the function
+     * @param canStartNext this is <code>false</code> if the <a href="#{@link}">{@link Track}</a> was interrupted(stopped on a new track was started)
+     * this
+     */
     open fun onTrackDone(track: Track, player: Player, canStartNext:Boolean){}
 
+    /**
+     * Called when a track fails to load
+     *
+     * @param track The track that that failed to load
+     * @param player The player that called the function
+     */
     open fun onLoadFailed(){}
 
+    /**
+     * Called when a track is paused
+     *
+     * @param track The track that is loaded
+     * @param player The player that called the function
+     */
     open fun onTrackPaused(track: Track, player: Player){}
 
+    /**
+     * Called when a track is resumed
+     *
+     * @param track The track that is loaded
+     * @param player The player that called the function
+     */
     open fun onTrackResumed(track: Track, player: Player){}
 
+    /**
+     * Called when a track fails to play
+     *
+     * @param track The track that failed to play
+     */
     open fun onTrackError(track: Track){}
 
 }
