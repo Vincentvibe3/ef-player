f750dc0be85db0e269e64ba079c2390c027b4468
Vincent <vincentvibe3@gmail.com>
2022-01-12 10:33:08 -0500

    Fix jitpack build

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.github.Vincentvibe3"
-version = "1.2.7"
+version = "1.2.8"
 
 repositories {
     mavenCentral()
@@ -18,7 +18,6 @@ dependencies {
     implementation("io.ktor:ktor-client-core:1.6.7")
     implementation("io.ktor:ktor-client-cio:1.6.7")
     implementation("org.json:json:20210307")
-    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.test {
