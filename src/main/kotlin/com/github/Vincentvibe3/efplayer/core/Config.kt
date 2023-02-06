054c137ef7767712571fde082ff20cf6c0573823
Vincent <vincentvibe3@gmail.com>
2023-02-05 19:16:34 -0500

    Fix config

--- src/main/kotlin/com/github/Vincentvibe3/efplayer/core/Config.kt
+++ src/main/kotlin/com/github/Vincentvibe3/efplayer/core/Config.kt
@@ -1,11 +1,12 @@
 package com.github.Vincentvibe3.efplayer.core
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import java.io.File
 
 @Serializable
 class Config(
-    var maxOpusChunks: Int = 4000,
+    @SerialName("maxChunks") var maxOpusChunks: Int = 4000,
     var spotifySecret: String = "",
     var spotifyClient: String = "",
     var spotifyToken: String = "",
