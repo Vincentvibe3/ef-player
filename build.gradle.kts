0f032ad6237bd24bb7b4098e70cb56a26e164e69
Vincent <vincentvibe3@gmail.com>
2024-08-31 22:17:36 -0400

    Update dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.8.0"
-    id("org.jetbrains.dokka") version "1.6.10"
+    id("org.jetbrains.dokka") version "1.9.2"
     kotlin("plugin.serialization") version "1.8.0"
     `maven-publish`
 }
